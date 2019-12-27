86df7252810f2fd90f55998a84e4a6f9e1f58c04
maiconmoretto <maiconmorettos@gmail.com>
2019-12-27 14:14:21 -0200

    Added person and button to change state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name:'Maicon', age:32 },
+      { name:'Maria', age:99 },
+      { name:'Jose', age:9999 }
+    ]
+  }
+
+  swithNameHandler = () =>  {
+    this.setState(
+      {
+        persons: [
+          { name:'Maicon1', age:10 },
+          { name:'Maria2', age:20 },
+          { name:'jose3', age:30 }
+        ]
+      }
+    )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.swithNameHandler}>click me ;)</button>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person> 
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const person = (props) => {
+  return <p> i am {props.name}, and have {props.age} age</p>;
+};
+
+export default person;
