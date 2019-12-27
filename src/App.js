6f6b2ea188ece262065b75207ca539737c2d1cd9
maiconmoretto <maiconmorettos@gmail.com>
2019-12-27 15:55:59 -0200

    Using the useState Hook for state manipulation

--- src/App.js
+++ src/App.js
@@ -1,39 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const App  = props => {
+const [ personsState, setPersonsState ] = useState({
     persons: [
       { name:'Maicon', age:32 },
       { name:'Maria', age:99 },
       { name:'Jose', age:9999 }
     ]
-  }
+  })
 
-  swithNameHandler = () =>  {
-    this.setState(
-      {
+  const swithNameHandler = () =>  {
+    setPersonsState({
         persons: [
           { name:'Maicon1', age:10 },
           { name:'Maria2', age:20 },
           { name:'jose3', age:30 }
         ]
-      }
-    )
+    })
   }
 
-  render() {
-    return (
-      <div className="App">
-        <button onClick={this.swithNameHandler}>click me ;)</button>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person> 
-      </div>
-    );
-  }
+  console.log(personsState)
+
+  return (
+    <div className="App ">
+      <button onClick={swithNameHandler}>click me ;)</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}></Person>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}></Person>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}></Person> 
+    </div>
+  );
 }
 
-
-export default App;
+export default App  ;
