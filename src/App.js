34b3d202968d77ec92e3bebbb00262d49ff642de
maiconmoretto <maiconmorettos@gmail.com>
2019-12-31 11:43:22 -0200

    Leasson-46: Passing Method References Between

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ const [ personsState, setPersonsState ] = useState({
     ]
   })
 
-  const swithNameHandler = () =>  {
+  const swithNameHandler = (newName) =>  {
     setPersonsState({
         persons: [
-          { name:'Maicon1', age:10 },
+          { name:  newName, age:10 },
           { name:'Maria2', age:20 },
           { name:'jose3', age:30 }
         ]
@@ -26,11 +26,21 @@ const [ personsState, setPersonsState ] = useState({
   return (
     <div className="App ">
       <button onClick={swithNameHandler}>click me ;)</button>
-        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}></Person>
-        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}></Person>
-        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}></Person> 
+        <Person 
+          name={personsState.persons[0].name} 
+          age={personsState.persons[0].age}
+          click={ () => swithNameHandler('Joseeeee')}>
+        </Person>
+        <Person 
+          name={personsState.persons[1].name} 
+          age={personsState.persons[1].age}>
+        </Person>
+        <Person 
+          name={personsState.persons[2].name} 
+          age={personsState.persons[2].age}>
+        </Person> 
     </div>
   );
 }
 
-export default App  ;
+export default App;
