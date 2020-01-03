04fab48c22e9634ffcde205060ddde8cea15c4df
maiconmoretto <maiconmorettos@gmail.com>
2020-01-03 17:23:28 -0200

    Adding Two Way Binding

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const person = (props) => {
-  return <p onClick={props.click}> i am {props.name}, and have {props.age} age</p>;
+  // return <p onClick={props.click}> i am {props.name}, and have {props.age} age</p>;
+  return (
+    <div>
+      <p onClick={props.click}> i am {props.name}, and have {props.age} age</p>
+      <p>props.children</p>
+      <input type="text" onChange={props.changed} value={props.name}/>
+    </div>
+    )
 };
 
 export default person;
