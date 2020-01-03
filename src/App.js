28137ad0ac681cf095b825ea33a74d0490c0c76c
maiconmoretto <maiconmorettos@gmail.com>
2020-01-03 17:23:28 -0200

    Leasson-47: Adding Two Way Binding

--- src/App.js
+++ src/App.js
@@ -21,23 +21,33 @@ const [ personsState, setPersonsState ] = useState({
     })
   }
 
-  console.log(personsState)
+  const nameChangeHandler = (event) => {
+    setPersonsState({
+      persons: [
+        { name:  'Maicon', age:10 },
+        { name:'Maria2', age:20 },
+        { name: event.target.value, age:30 }
+      ]
+  })
+  }
 
   return (
     <div className="App ">
       <button onClick={swithNameHandler}>click me ;)</button>
-        <Person 
+        <Person   
           name={personsState.persons[0].name} 
           age={personsState.persons[0].age}
           click={ () => swithNameHandler('Joseeeee')}>
         </Person>
         <Person 
           name={personsState.persons[1].name} 
-          age={personsState.persons[1].age}>
+          age={personsState.persons[1].age}
+          click={swithNameHandler.bind(this, 'MAX')}>
         </Person>
         <Person 
           name={personsState.persons[2].name} 
-          age={personsState.persons[2].age}>
+          age={personsState.persons[2].age}
+          changed={nameChangeHandler}>
         </Person> 
     </div>
   );
