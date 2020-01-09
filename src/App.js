9e09e9322ce3b85a465a8a2c5bb5caa7c92f4b09
maiconmoretto <maiconmorettos@gmail.com>
2020-01-09 16:26:07 -0200

    Leasson - 58. Updating State Immutably - coping the original array

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
   };
 
   deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons;
+    const persons = this.state.persons.slice();
     persons.splice(personIndex, 1);
     this.setState({persons: persons})
   }
